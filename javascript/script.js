4acc8b6ab218641fce4fbbc08cac3943543887cc
minw0525 <74699528+minw0525@users.noreply.github.com>
2020-11-20 04:18:39 +0900

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,72 @@
+const paramsObj = {};
+const paramReg = /(\?|&)(\D|\d){1,}/;
+let filePath;
+let pageIdx;
+let currLang;
+let dataSheet;
+let url = window.location.href;
+
+
+function checkUrl(url){
+	const currParam = paramReg.exec(url);
+	return new Promise((resolve, reject)=>{
+		//get querystring
+		function getParam(){
+			paramsObj.lang = "";
+			if(currParam){
+				currParam.replace(
+					/[?&]+([^=&]+)=([^&]*)/gi, function(str, key, value) { paramsObj[key] = value; }
+				);
+			}
+	    console.log(paramsObj);
+	    currLang = paramsObj.lang;
+		}
+
+		//check pathname
+		filePath = window.location.pathname;
+		switch (filePath) {
+			case '/':
+				pageIdx = 1;
+				break;
+
+			case '/project':
+				pageIdx = 2;
+				break;
+
+			case '/credit':
+				pageIdx = 3;
+				break;
+
+			//default: window.location.href = "https://digdeep.works"
+		}
+		getParam();
+		(currLang !== "en") ? resolve(paramsObj) : reject(paramsObj);
+	});
+}
+
+const removeLang = params => {
+  console.log(url)
+  url = url.replace(langPart,"");
+  currLang = "ko";
+  console.log(url);
+  console.log (currLang);
+  window.location.href = url;
+  return params;
+}
+
+const keepLang = params => {
+  currLang = "ko";
+  console.log(url);
+  return params;
+}
+
+
+checkUrl(url)
+    .then(p=>{
+      p.lang ? removeLang(p) : keepLang(p)
+      }
+    )
+
+console.log(paramsObj);
+console.log(currLang);
+console.log(pageIdx);
