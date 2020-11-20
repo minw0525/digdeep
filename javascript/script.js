a405fca49d2bed00ebcf638b484c2e29dcd58eb4
minw0525 <74699528+minw0525@users.noreply.github.com>
2020-11-20 22:28:01 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -25,7 +25,7 @@ function checkUrl(url){
 		//check pathname
 		filePath = window.location.pathname;
 		switch (filePath) {
-			case "/digdeep/":
+			case "/digdeep/index.html":
 				pageIdx = 1;
 				break;
 
@@ -100,10 +100,12 @@ function getData(lang){
 	console.log(dataSheet);
 }
 
+// draw initial div in grid-container
+function contentDraw(){
 
+}
 
-
-
+//promise chain
 checkUrl(url)
     .then(p=>{
       p.lang ? removeLang(p) : keepLang(p)
